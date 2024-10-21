b823fad0bbff9ebb3849260d4e8c97478e7f71aa
sirdashasan <sirdashasan@hotmail.com>
2024-10-21 19:06:10 +0300

    Navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <nav>Navbar</nav>
-      <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
+      <Navbar />
+      <main className="min-h-screen max-w-screen-xl mx-auto px-4 py-6 font-primary">
         <Outlet />
       </main>
       <footer>Footer</footer>
