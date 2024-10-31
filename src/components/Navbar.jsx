6cc3580557940f37ae40d142d35777c75ddcb318
sirdashasan <sirdashasan@hotmail.com>
2024-10-31 10:40:10 +0300

    Login page added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const navigation = [
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
-  const currentUser = true;
+  const currentUser = false;
 
   return (
     <header className="max-w-screen-2xl mx-auto px-4 md:px-20 py-6">
