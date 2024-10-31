b380e587ee350a6861ba8bc054091f037cec8878
sirdashasan <sirdashasan@hotmail.com>
2024-10-31 10:54:51 +0300

    React Hook Form added

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,16 +1,27 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { FaGoogle } from "react-icons/fa";
+import { useForm } from "react-hook-form";
 
 const Login = () => {
   const [message, setMessage] = useState("");
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => console.log(data);
+
+  const handleGoogleSignIn = () => {};
 
   return (
     <div className="h-[calc(100vh-120px)] flex justify-center items-center">
       <div className="w-full max-w-sm mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <h2 className="text-xl font-semibold mb-4">Please Login</h2>
 
-        <form>
+        <form onSubmit={handleSubmit(onSubmit)}>
           <div className="mb-4 ">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -19,6 +30,7 @@ const Login = () => {
               Email
             </label>
             <input
+              {...register("email", { required: true })}
               type="email"
               name="email"
               id="email"
@@ -35,6 +47,7 @@ const Login = () => {
               Password
             </label>
             <input
+              {...register("password", { required: true })}
               type="password"
               name="password"
               id="password"
@@ -63,7 +76,10 @@ const Login = () => {
 
         {/* google sign in */}
         <div className="mt-4">
-          <button className="w-full flex flex-wrap gap-1 items-center justify-center bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none">
+          <button
+            onClick={handleGoogleSignIn}
+            className="w-full flex flex-wrap gap-1 items-center justify-center bg-secondary hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none"
+          >
             <FaGoogle className="mr-2" />
             Sign in with Google
           </button>
